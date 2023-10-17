a555a0963fefef105bd1017307dd6dae19c7e57a
SayaleeDamle <sayalee.damle@onepointltd.com>
2023-10-17 13:40:28 +0530

    add exit feature

--- image_generator_bot/frontend/main.py
+++ image_generator_bot/frontend/main.py
@@ -5,8 +5,10 @@ from image_generator_bot.config import cfg
 import image_generator_bot.backend.image_generator as image_g
 from image_generator_bot.log_factory import logger
 
+
 def is_yes(input_msg: str) -> bool:
     return input_msg in ("yes", "y", "Yes", "Y")
+
 
 @cl.on_chat_start
 async def start() -> cl.Message:
@@ -15,22 +17,35 @@ async def start() -> cl.Message:
         prompt = await image_g.give_prompt(content_of_image["content"])
         logger.info(prompt)
         await cl.Message(content=f"{prompt}").send()
-        prompt_satisfied = await ask_user_msg("Are you satisfied with the prompt generated?")
-        if is_yes(prompt_satisfied['content']):
+        prompt_satisfied = await ask_user_msg(
+            "Are you satisfied with the prompt generated?"
+        )
+        if is_yes(prompt_satisfied["content"]):
             while True:
-                
                 image = await image_g.generate_advice_image(prompt)
                 await cl.Message(content="Look at the image here! click the URL").send()
                 await cl.Message(content=f"{image}").send()
-                image_satisfied = await ask_user_msg("Are you satisfied with the image generated?")
-                if is_yes(image_satisfied['content']):
-                    await cl.Message(content="Thank You! Download this image", elements=image).send()
+                image_satisfied = await ask_user_msg(
+                    "Are you satisfied with the image generated?"
+                )
+                if is_yes(image_satisfied["content"]):
+                    await cl.Message(
+                        content="Thank You! Download this image", elements=image
+                    ).send()
+                    break
+                elif image_satisfied["content"] == "exit":
+                    await cl.Message(content="thank you").send()
                     break
                 else:
                     await cl.Message(content="I will try again").send()
                     continue
+        elif prompt_satisfied["content"] == "exit":
+            await cl.Message(content="thank you").send()
+            break
         else:
             await cl.Message(content="I will try again").send()
+            continue
+
 
 async def ask_user_msg(question) -> AskFileResponse:
     ans = None
@@ -39,5 +54,3 @@ async def ask_user_msg(question) -> AskFileResponse:
             content=f"{question}", timeout=cfg.ui_timeout
         ).send()
     return ans
-
-    
